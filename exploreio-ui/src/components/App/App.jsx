2e31adf3c1a8c7c92c8c74b15a487f1af5b6304e
dinacazun <dinacazun02@gmail.com>
2023-07-11 17:34:58 -0400

    navbar

--- exploreio-ui/src/components/App/App.jsx
+++ exploreio-ui/src/components/App/App.jsx
@@ -1,9 +1,18 @@
 import "./App.css"
 import React from "react"
+import { useState, useEffect } from "react"
+import NavBar from "../NavBar/NavBar"
 import { ReactDOM } from "react"
+import { BrowserRouter } from "react-router-dom"
 
 export default function App () {
+    const [loggedIn, setLoggedIn] = useState(false)
+
     return(
-        <div><p>Hello!</p></div>
+        <div className="app">
+            <BrowserRouter>
+                <NavBar isLoggedIn={loggedIn}/>
+            </BrowserRouter>
+        </div>
     )
 }
