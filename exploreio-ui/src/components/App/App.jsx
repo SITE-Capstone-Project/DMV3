3256b6bf295afe983cdbb4c8a6a8d395dc097ca4
dinacazun <dinacazun02@gmail.com>
2023-07-11 17:58:26 -0400

    upd8

--- exploreio-ui/src/components/App/App.jsx
+++ exploreio-ui/src/components/App/App.jsx
@@ -2,6 +2,7 @@ import "./App.css"
 import React from "react"
 import { useState, useEffect } from "react"
 import NavBar from "../NavBar/NavBar"
+import Home from "../Home/Home"
 import { ReactDOM } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
