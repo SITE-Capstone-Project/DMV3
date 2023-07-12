124c9c09539bc20da61b56479b223ad4fbd994af
aboss908 <aboss908@gmail.com>
2023-07-11 23:10:06 -0400

    Modified the Home page

--- exploreio-ui/src/components/NavBar/NavBar.jsx
+++ exploreio-ui/src/components/NavBar/NavBar.jsx
@@ -7,23 +7,26 @@ export default function NavBar({isLoggedIn}){
     return(
         <nav className = "navbar">
             <img src = {logo}/>
-            <Link className = "link" to = "/">Home</Link>
-            <Link className = "link" to = "/destinations">Destinations</Link>
-            <Link className = "link" to = "/packages">Packages</Link>
-            {isLoggedIn?(
-                <Link className = "link" to = "/">
-                <button className = "signOut">Sign Out</button>
-                </Link>
-            ):(
-            <div>
-                <Link className = "link" to = "/login">
-                <button className = "signIn">Sign In</button>
-                </Link>
-                <Link className = "link" to = "/register">
-                <button className = "register">Register</button>
-                </Link>
+
+            <div className ="other">
+                {isLoggedIn?(
+                    <Link className = "link" to = "/">
+                    <button className = "signOut">Sign Out</button>
+                    </Link>
+                ):(
+                <div>
+                    <Link className = "link" to = "/login">
+                    <button className = "signIn">Sign In</button>
+                    </Link>
+                    <Link className = "link" to = "/register">
+                    <button className = "register">Register</button>
+                    </Link>
+                </div>
+                )}
+                <Link className = "link adjust" to = "/packages">Packages</Link>
+                <Link className = "link adjust" to = "/destinations">Destinations</Link>
+                <Link className = "link adjust" to = "/">Home</Link>
             </div>
-            )}
         </nav>
     )
 }
