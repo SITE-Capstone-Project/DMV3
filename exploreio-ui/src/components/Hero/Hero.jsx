98fa4b9605c71b6c340b8b9d8102214b95d9fd48
emandaseifu <emandatseifu@gmail.com>
2023-07-13 18:23:28 -0400

    hero

--- exploreio-ui/src/components/Hero/Hero.jsx
+++ exploreio-ui/src/components/Hero/Hero.jsx
@@ -9,7 +9,8 @@ export default function Hero() {
           <h1>Exploreio</h1>
         </div>
         <div className="blurb">
-          <h3>Welcome to Exploreio, the gateway to global adventure! Embark on a virtual journey like no other as our meticulously curated travel app unveils the world's hidden gems. Whether you're dreaming of the vibrant beaches of Miami or the bustling streets of Seoul, our app is here to help you curate your perfect vacation. Exploreio takes care of all your travel needs, from finding the best flights to booking the perfect hotel for your chosen destination. Get ready to embark on an unforgettable journey with Exploreio, where your dream vacation is just a few taps away.</h3>
+          <p>Welcome to Exploreio, the gateway to global adventure! </p>
+          <p>Embark on a virtual journey like no other as our meticulously curated travel app unveils the world's hidden gems. Whether you're dreaming of the vibrant beaches of Miami or the bustling streets of Seoul, our app is here to help you curate your perfect vacation. Exploreio takes care of all your travel needs, from finding the best flights to booking the perfect hotel for your chosen destination. Get ready to embark on an unforgettable journey with Exploreio, where your dream vacation is just a few taps away.</p>
         </div>
       </div>
     </div>
