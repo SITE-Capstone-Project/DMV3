4b0ed5559f4d7627574085e0896971e1b033ff94
emandaseifu <emandatseifu@gmail.com>
2023-07-14 16:04:58 -0400

    footer

--- exploreio-ui/src/components/App/App.jsx
+++ exploreio-ui/src/components/App/App.jsx
@@ -1,22 +1,24 @@
-import "./App.css"
-import React from "react"
-import { useState, useEffect } from "react"
-import NavBar from "../NavBar/NavBar"
-import Home from "../Home/Home"
-import { ReactDOM } from "react"
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import "./App.css";
+import React, { useState } from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import NavBar from "../NavBar/NavBar";
+import Home from "../Home/Home";
+import Destinations from "../Destinations/Destinations";
+import SearchBox from "../SearchBox/SearchBox";
 
-export default function App(){
-    const [loggedIn, setLoggedIn] = useState(false)
-    return(
+export default function App() {
+    const [loggedIn, setLoggedIn] = useState(false);
+
+    return (
         <div>
             <BrowserRouter>
-                <NavBar isLoggedIn={loggedIn}/>
+                <NavBar isLoggedIn={loggedIn} />
                 <Routes>
-                    <Route path = "/" element = {<Home/>}/>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/destinations" element={<Destinations />} />
                 </Routes>
             </BrowserRouter>
         </div>
-    )
+    );
 }
-    
+
