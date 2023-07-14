6829589b7bd13a1c711494ba9072111357ceb8f1
aboss908 <aboss908@gmail.com>
2023-07-14 17:03:27 -0400

    small-update

--- exploreio-ui/src/components/SlideCard/SlideCard.jsx
+++ exploreio-ui/src/components/SlideCard/SlideCard.jsx
@@ -6,6 +6,13 @@ import logo from "../../assets/logo.png"
 export default function SlideCard(){
     return(
         <div className = "sliding-card">
+            <div className="slidecard">
+                <img src = {logo}/>
+                <div className="titleRating">
+                    <h2>Los Angeles</h2>
+                    <p>Rating: 3/5</p>
+                </div>
+            </div>
         </div>
     )
 }
