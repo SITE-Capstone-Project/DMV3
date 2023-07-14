061311523256a6a01e31bc48b10ece11a41e28a7
aboss908 <aboss908@gmail.com>
2023-07-14 15:59:27 -0400

    Made some sliding-cards

--- exploreio-ui/src/components/SlideCard/SlideCard.jsx
+++ exploreio-ui/src/components/SlideCard/SlideCard.jsx
@@ -2,19 +2,31 @@ import "./SlideCard.css"
 import React from "react"
 import { Link } from "react-router-dom"
 import logo from "../../assets/logo.png"
+import Carousel from 'react-grid-carousel'
 
 export default function SlideCard(){
     return(
-        <div className="slidecardpage">
-        <div className="slidingContainer">
+        <div className = "sliding-card">
+        <Carousel cols={6} rows={1} gap={0} loop>
+        <Carousel.Item>
             <div className="slidecard">
-                <img src = {logo}/>
-                <div className="titleRating">
-                    <h2>Los Angeles</h2>
-                    <p>Rating: 3/5</p>
-                </div>
+                    <img src = {logo}/>
+                    <div className="titleRating">
+                        <h2>Los Angeles</h2>
+                        <p>Rating: 3/5</p>
+                    </div>
             </div>
-
+        </Carousel.Item>
+        <Carousel.Item>
+            <div className="slidecard">
+                    <img src = {logo}/>
+                    <div className="titleRating">
+                        <h2>Los Angeles</h2>
+                        <p>Rating: 3/5</p>
+                    </div>
+            </div>
+        </Carousel.Item>
+        <Carousel.Item>
             <div className="slidecard">
                 <img src = {logo}/>
                 <div className="titleRating">
@@ -22,15 +34,17 @@ export default function SlideCard(){
                     <p>Rating: 3.6/5</p>
                 </div>
             </div>
-
+        </Carousel.Item>
+        <Carousel.Item>
             <div className="slidecard">
                 <img src = {logo}/>
                 <div className="titleRating">
                     <h2>Washington, DC</h2>
                     <p>Rating: 3/5</p>
                 </div>
-            </div>
-
+            </div>            
+        </Carousel.Item>
+        <Carousel.Item>
             <div className="slidecard">
                 <img src = {logo}/>
                 <div className="titleRating">
@@ -38,7 +52,8 @@ export default function SlideCard(){
                     <p>Rating: 3.7/5</p>
                 </div>
             </div>
-
+        </Carousel.Item>
+        <Carousel.Item>
             <div className="slidecard">
                 <img src = {logo}/>
                 <div className="titleRating">
@@ -46,23 +61,101 @@ export default function SlideCard(){
                     <p>Rating: 4/5</p>
                 </div>
             </div>
-
-            <div className="slidecard">
+        </Carousel.Item>
+        <Carousel.Item>
+        <div className="slidecard">
                 <img src = {logo}/>
                 <div className="titleRating">
                     <h2>Silver Spring, MD</h2>
                     <p>Rating: 3/5</p>
                 </div>
             </div>
-
-            <div className="slidecard">
+        </Carousel.Item>
+        <Carousel.Item>
+        <div className="slidecard">
                 <img src = {logo}/>
                 <div className="titleRating">
                     <h2>Hello, World</h2>
                     <p>Rating: 3/5</p>
                 </div>
             </div>
-        </div>
+        </Carousel.Item>
+        </Carousel>
+
+        <Carousel cols={6} rows={1} gap={0} loop>
+        <Carousel.Item>
+            <div className="slidecard">
+                    <img src = {logo}/>
+                    <div className="titleRating">
+                        <h2>Los Angeles</h2>
+                        <p>Rating: 3/5</p>
+                    </div>
+            </div>
+        </Carousel.Item>
+        <Carousel.Item>
+            <div className="slidecard">
+                    <img src = {logo}/>
+                    <div className="titleRating">
+                        <h2>Los Angeles</h2>
+                        <p>Rating: 3/5</p>
+                    </div>
+            </div>
+        </Carousel.Item>
+        <Carousel.Item>
+            <div className="slidecard">
+                <img src = {logo}/>
+                <div className="titleRating">
+                    <h2>New York</h2>
+                    <p>Rating: 3.6/5</p>
+                </div>
+            </div>
+        </Carousel.Item>
+        <Carousel.Item>
+            <div className="slidecard">
+                <img src = {logo}/>
+                <div className="titleRating">
+                    <h2>Washington, DC</h2>
+                    <p>Rating: 3/5</p>
+                </div>
+            </div>            
+        </Carousel.Item>
+        <Carousel.Item>
+            <div className="slidecard">
+                <img src = {logo}/>
+                <div className="titleRating">
+                    <h2>Nashville, TN</h2>
+                    <p>Rating: 3.7/5</p>
+                </div>
+            </div>
+        </Carousel.Item>
+        <Carousel.Item>
+            <div className="slidecard">
+                <img src = {logo}/>
+                <div className="titleRating">
+                    <h2>San Francisco, CA</h2>
+                    <p>Rating: 4/5</p>
+                </div>
+            </div>
+        </Carousel.Item>
+        <Carousel.Item>
+        <div className="slidecard">
+                <img src = {logo}/>
+                <div className="titleRating">
+                    <h2>Silver Spring, MD</h2>
+                    <p>Rating: 3/5</p>
+                </div>
+            </div>
+        </Carousel.Item>
+        <Carousel.Item>
+        <div className="slidecard">
+                <img src = {logo}/>
+                <div className="titleRating">
+                    <h2>Hello, World</h2>
+                    <p>Rating: 3/5</p>
+                </div>
+            </div>
+        </Carousel.Item>
+        </Carousel>
         </div>
     )
 }
