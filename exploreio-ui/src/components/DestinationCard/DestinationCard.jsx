b1a451f7996713c52aacd0bcc397f93d7c140528
dinacazun <dinacazun02@gmail.com>
2023-07-17 13:50:51 -0400

    destinations main tab with destinations container

---
+++ exploreio-ui/src/components/DestinationCard/DestinationCard.jsx
@@ -0,0 +1,14 @@
+import "./DestinationCard.css"
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
+import logo from "../../assets/logo.png"
+
+export default function DestinationCard() {
+    <div className="destinationCard">
+        <img src={logo} />
+        <div className="titleRating">
+            <h2>Los Angeles</h2>
+            <p>Rating: 3/5</p>
+        </div>
+    </div>
+}
---
+++ exploreio-ui/src/components/DestinationContainer/DestinationContainer.jsx
@@ -0,0 +1,28 @@
+import "./DestinationContainer.css"
+import React, { useState } from "react"
+import { Link } from "react-router-dom"
+import logo from "../../assets/logo.png"
+import DestinationCard from "../DestinationCard/DestinationCard"
+
+export default function DestinationContainer() {
+    const [showMore, setShowMore] = useState(false);
+    return (
+        <div className="destinationContainer">
+            <div className="container1">
+                <DestinationCard/>
+                <DestinationCard/>
+                <DestinationCard/>
+                <DestinationCard/>
+                <DestinationCard/>
+            </div>
+            <div className="container2">
+                <DestinationCard/>
+                <DestinationCard/>
+                <DestinationCard/>
+                <DestinationCard/>
+                <DestinationCard/>
+            </div>
+            <button className="showMoreButton" onClick={() => setShowMore(!showMore)}>Show More</button>
+        </div>
+    );
+}
