9733eaace695a4e63eb1c0805edab5dc030eecfc
emandaseifu <emandatseifu@gmail.com>
2023-07-17 13:32:13 -0400

    update

--- exploreio-ui/src/components/SlideCard/SlideCard.jsx
+++ exploreio-ui/src/components/SlideCard/SlideCard.jsx
@@ -1,49 +1,109 @@
-import "./SlideCard.css";
-import React, { useState, useEffect } from 'react';
+import "./SlideCard.css"
+import React from "react"
+import { Link } from "react-router-dom"
+import logo from "../../assets/logo.png"
 
-const SlideCard = () => {
-    const images = [
-        'https://www.traveloffpath.com/wp-content/uploads/2021/10/Encouraging-Travel-And-Hotel-Occupancy-Figures-Project-A-Busy-Winter-Of-Travel.jpg',
-        'https://grandcircle.scene7.com/is/image/GrandCircle/T4947/4x3/600',
-        'https://images.squarespace-cdn.com/content/v1/53b839afe4b07ea978436183/0b5ca971-1eca-484f-9a84-071b23ba5126/small-towns-in-france.jpg',
-        'https://www.traveloffpath.com/wp-content/uploads/2021/10/Encouraging-Travel-And-Hotel-Occupancy-Figures-Project-A-Busy-Winter-Of-Travel.jpg',
-        'https://grandcircle.scene7.com/is/image/GrandCircle/T4947/4x3/600',
-        'https://images.squarespace-cdn.com/content/v1/53b839afe4b07ea978436183/0b5ca971-1eca-484f-9a84-071b23ba5126/small-towns-in-france.jpg',
-    ];
-
-    const [slideIndex, setSlideIndex] = useState(0);
-    const slideWidth = 50; // Set to 100 to move one image at a time
-
-    const showSlide = (index) => {
-        setSlideIndex(index);
-    };
-
-    const nextSlide = () => {
-        setSlideIndex((slideIndex + 1) % images.length);
-    };
-
-    const prevSlide = () => {
-        setSlideIndex((slideIndex - 1 + images.length) % images.length);
-    };
-
-    useEffect(() => {
-        const interval = setInterval(nextSlide, 5000);
-        return () => clearInterval(interval);
-    }, []);
-
-    return (
-        <div className="slide-card-container">
-          <div className="carousel-container">
-            <div className="slides" style={{ transform: `translateX(-${slideIndex * slideWidth}%)` }}>
-              {images.map((image, index) => (
-                <img key={index} src={image} alt={`Image ${index + 1}`} />
-              ))}
-            </div>
+export default function SlideCard() {
+  return (
+    <div className="slidecardpage">
+      <div className="slidingContainer">
+        <div className="slidecard">
+          <img src={logo} />
+          <div className="titleRating">
+            <h2>Los Angeles</h2>
+            <p>Rating: 3/5</p>
           </div>
-          <button className="prev-btn" onClick={prevSlide} disabled={slideIndex === 0}>Previous</button>
-          <button className="next-btn" onClick={nextSlide} disabled={slideIndex === images.length - 1}>Next</button>
         </div>
-      );
-    };
 
-export default SlideCard;
+        <div className="slidecard">
+          <img src={logo} />
+          <div className="titleRating">
+            <h2>New York</h2>
+            <p>Rating: 3.6/5</p>
+          </div>
+        </div>
+
+        <div className="slidecard">
+          <img src={logo} />
+          <div className="titleRating">
+            <h2>Washington, DC</h2>
+            <p>Rating: 3/5</p>
+          </div>
+        </div>
+
+        <div className="slidecard">
+          <img src={logo} />
+          <div className="titleRating">
+            <h2>Nashville, TN</h2>
+            <p>Rating: 3.7/5</p>
+          </div>
+        </div>
+
+        <div className="slidecard">
+          <img src={logo} />
+          <div className="titleRating">
+            <h2>San Francisco, CA</h2>
+            <p>Rating: 4/5</p>
+          </div>
+        </div>
+
+        <div className="slidecard">
+          <img src={logo} />
+          <div className="titleRating">
+            <h2>Silver Spring, MD</h2>
+            <p>Rating: 3/5</p>
+          </div>
+        </div>
+
+        <div className="slidecard">
+          <img src={logo} />
+          <div className="titleRating">
+            <h2>Hello, World</h2>
+            <p>Rating: 3/5</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+// export const Carousel = () => {
+//     return (
+//         <div className="carouselslide" data-ride="carousel">
+//             <div className="carousel-inner">
+//                 <div className="carousel-item active">
+//                     <SlideCard />
+//                 </div>
+//                 <div className="carousel-item">
+//                     <SlideCard />
+//                 </div>
+//                 <div className="carousel-item">
+//                     <SlideCard />
+//                 </div>
+//             </div>
+
+//             <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+//                     <span className="carousel-control-prev-icon" aria-hidden="true"></span>
+//                     <span className="sr-only">Previous</span>
+//             </a>Name
+//             <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+//                     <span className="carousel-control-next-icon" aria-hidden="true"></span>
+//                     <span className="sr-only">Next</span>
+//             </a>
+//         </div>
+//     )
+// }
+
+// const SlideCard = () => {
+//     return (
+//         <div className='slide-card'>
+//             <div className="slidecard">
+//                  <img src = {logo}/>
+//                  <div className="titleRating">
+//                      <h2>Los Angeles</h2>
+//                      <p>Rating: 3/5</p>
+//                 </div>
+//              </div>
+//         </div>
+//     )
+// }
