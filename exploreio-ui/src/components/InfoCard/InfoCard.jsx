9411b690a55128bb6dc33c3e0c05a6fdd5a50eda
aboss908 <aboss908@gmail.com>
2023-07-18 13:25:11 -0400

    Added test element

--- exploreio-ui/src/components/InfoCard/InfoCard.jsx
+++ exploreio-ui/src/components/InfoCard/InfoCard.jsx
@@ -3,6 +3,15 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
 
+//Los Angeles
+//4
+//"Located in California, it is the second-largest city in the United States and is renowned for the entertainment industry, including Hollywood, as well as its pleasant climate and beautiful beaches."
+//https://images.pexels.com/photos/3006222/pexels-photo-3006222.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1
+//https://images.pexels.com/photos/2525903/pexels-photo-2525903.jpeg
+//NA
+//USA
+//LAX
+
 export default function InfoCard(){
     return(
         <div className="infocard">
