13e6b7d22241b937d9acf8ec6df8dd17d52fd0de
emandaseifu <emandatseifu@gmail.com>
2023-07-18 17:31:10 -0400

    search

--- exploreio-ui/src/components/SearchBox/SearchBox.jsx
+++ exploreio-ui/src/components/SearchBox/SearchBox.jsx
@@ -2,10 +2,16 @@ import React, { useState } from 'react';
 
 export default function SearchBox({ data, onFilter }) {
   const [searchTerm, setSearchTerm] = useState('');
+  const [filterOptions, setFilterOptions] = useState(''); // Add state for filtering options
 
   const handleInputChange = (event) => {
     setSearchTerm(event.target.value);
-    onFilter(event.target.value);
+    onFilter(event.target.value, filterOptions); // Pass filter options to the onFilter function
+  };
+
+  const handleFilterChange = (event) => {
+    setFilterOptions(event.target.value);
+    onFilter(searchTerm, event.target.value); // Pass search term and filter options to the onFilter function
   };
 
   return (
@@ -16,6 +22,34 @@ export default function SearchBox({ data, onFilter }) {
         value={searchTerm}
         onChange={handleInputChange}
       />
+
+      <select value={filterOptions} onChange={handleFilterChange}>
+        <option value="">Filter by:</option>
+        <option value="rating">Rating</option>
+        <option value="region">Region</option>
+      </select>
     </div>
   );
 }
+
+// import React, { useState } from 'react';
+
+// export default function SearchBox({ data, onFilter }) {
+//   const [searchTerm, setSearchTerm] = useState('');
+
+//   const handleInputChange = (event) => {
+//     setSearchTerm(event.target.value);
+//     onFilter(event.target.value);
+//   };
+
+//   return (
+//     <div className='search'>
+//       <input
+//         type="text"
+//         placeholder="Search..."
+//         value={searchTerm}
+//         onChange={handleInputChange}
+//       />
+//     </div>
+//   );
+// }
