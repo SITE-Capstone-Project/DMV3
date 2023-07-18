6b986cabd585beef8d32eaf9c03dc58fba34cd48
aboss908 <aboss908@gmail.com>
2023-07-18 18:32:26 -0400

    Working on info card page

--- exploreio-ui/src/components/InfoCard/InfoCard.jsx
+++ exploreio-ui/src/components/InfoCard/InfoCard.jsx
@@ -2,15 +2,8 @@ import "./InfoCard.css";
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
-
-//Los Angeles
-//4
-//"Located in California, it is the second-largest city in the United States and is renowned for the entertainment industry, including Hollywood, as well as its pleasant climate and beautiful beaches."
-//https://images.pexels.com/photos/3006222/pexels-photo-3006222.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1
-//https://images.pexels.com/photos/2525903/pexels-photo-2525903.jpeg
-//NA
-//USA
-//LAX
+import FlightForm from "../FlightForm/FlightForm.jsx";
+import Hotels from "../Hotels/Hotels";
 
 const apiKEY = "AIzaSyCB-kma-hpj9XAX-cSDoecd36eiABcPGEw";
 const string = "Located in California, it is the second-largest city in the United States and is renowned for the entertainment industry, including Hollywood, as well as its pleasant climate and beautiful beaches."
@@ -22,7 +15,7 @@ export default function InfoCard(){
 
             <div className = "intro-info">
                 <div className = "dest-info">
-                    <h1 className = "title">Los Angeles, USA</h1>
+                    <h1 className = "title"> Los Angeles, USA </h1>
                     <div className="loc-info">
                         <p>{string}</p>
                     </div>
@@ -31,7 +24,6 @@ export default function InfoCard(){
                     <iframe
                         width="550"
                         height="500"
-                        loading="lazy"
                         referrerPolicy="no-referrer-when-downgrade"
                         src={`https://www.google.com/maps/embed/v1/place?key=${apiKEY}&q=Los Angeles`}>
                     </iframe>
@@ -40,22 +32,18 @@ export default function InfoCard(){
 
             <div className ="full-container">
                 <div className="infoAndActivities">
-                    <h2>Activities</h2>
-                    <p>Flight goes here</p>                    
+                    <h2 className = "activities-title">Activities</h2>
+                    <p>There are plenty of activities that you can do in <span id="cityname">Los Angeles!</span> </p> 
+                    <p>Here are a few:</p>                    
                 </div>
 
                 <div className="flightsAndHotels">
-
-                    <div className="flights">
-                        <h2>Flights</h2>
-                        <p>Flight goes here</p>
-                    </div>
-
                     <div className="hotels">
-                        <h2>Hotels</h2>
-                        <p>Hotels go here</p>
+                        <Hotels/>
                     </div>
-
+                    <div className="flights">
+                        <FlightForm/>
+                    </div>
                 </div>
             </div>
 
