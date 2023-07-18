f290d5f0911079b5771542147ef36fd4555b3163
emandaseifu <emandatseifu@gmail.com>
2023-07-18 16:35:34 -0400

    search

--- exploreio-ui/src/components/SearchBox/SearchBox.jsx
+++ exploreio-ui/src/components/SearchBox/SearchBox.jsx
@@ -9,7 +9,7 @@ export default function SearchBox({ data, onFilter }) {
   };
 
   return (
-    <div>
+    <div className='search'>
       <input
         type="text"
         placeholder="Search..."
