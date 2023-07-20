fdb00ec27d2b141224e72504e35d6e3f9a4c250f
aboss908 <aboss908@gmail.com>
2023-07-20 19:09:20 -0400

    Made lots of changes to InfoCard

--- exploreio-ui/src/components/Hotels/Hotels.jsx
+++ exploreio-ui/src/components/Hotels/Hotels.jsx
@@ -1,10 +1,64 @@
 import React from "react"
+import { useState } from "react"
 import "./Hotels.css"
 
-export default function Hotels() {
+export default function Hotels({hotels}) {
     return (
         <div className = "hotels-list">
             <h2 className = "hotels-title"> Hotels </h2>
+            {hotels?.length != 0 && 
+            (<div>
+                {hotels?.map((element, index) => {
+                    return <HotelCard 
+                    key = {element + index}
+                    name = {element.name}
+                    rating = {element.rating}
+                    priceLevel = {element.price_level}
+                    description = {element.description}
+                    web_url = {element.web_url}/>
+                })}
+            </div>)}
+
+        </div>
+    )
+}
+
+export function HotelCard({name, priceLevel, rating, description, web_url}) {
+    const[isHover, setIsHover] = useState(false)
+
+    const formatString = (string, num) => {
+        let formatted = string
+        if (formatted?.length <= num) {
+        } else {
+            formatted = string?.substring(0, num)
+            if (formatted?.charAt(formatted.length - 1) == " ") {
+                formatted = string?.substring(0, num - 1)
+            }
+            formatted += ".."
+        }
+
+        return formatted
+    }
+
+    return (
+        <div className = "hotel-card" onMouseEnter = {() => { setIsHover(true)}} onMouseLeave={() => {setIsHover(false)}}>
+            {isHover && (
+                <div className = "popup-description">
+                    <p id = "hotel-title-popup"> {name} </p>
+                    <p> {description} </p>
+                </div>
+            )}
+            <div className = "hotel-intro">
+                <p id = "hotel-title"> 
+                <a href={web_url}> {formatString(name, 17)} </a>
+                </p>
+            </div>
+            <div className = "hotel-details">
+            <p id = "hotel-pricerange"> {priceLevel} </p>
+                <img id = "rating-star" src={`https://purepng.com/public/uploads/large/purepng.com-green-starstargeometricallydecagonconcavestardomclipartgreen-1421526503700o6cix.png`}/>
+                <p id = "hotel-rating"> {rating} </p>
+                <p id = "description"> {formatString(description, 20)}</p>
+            </div>
         </div>
     )
 }
--- exploreio-ui/src/components/InfoCard/InfoCard.jsx
+++ exploreio-ui/src/components/InfoCard/InfoCard.jsx
@@ -1,45 +1,66 @@
 import "./InfoCard.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
 import FlightForm from "../FlightForm/FlightForm.jsx";
 import Hotels from "../Hotels/Hotels";
+import Activities from "../Activities/Activities";
 
-const apiKEY = "AIzaSyCB-kma-hpj9XAX-cSDoecd36eiABcPGEw";
-const string = "Located in California, it is the second-largest city in the United States and is renowned for the entertainment industry, including Hollywood, as well as its pleasant climate and beautiful beaches."
+const apiKEY = "";
 
 export default function InfoCard(){
+    const[map, setMap] = useState(false)
+    const[destination, setDestination] = useState([])
+
+    const findInfo = async () => {
+        const response = await fetch("http://localhost:3001/exploreio/destinations/15");
+        const responseJson = await response.json();
+        setDestination(responseJson)
+    }
+
+    useEffect(() => {
+        findInfo()
+    }, [])
+
+    const loadMap = async () => {
+        setMap(true)
+    }
+
+    setTimeout(loadMap, 2000)
+
     return(
         <div className="infocard">
-            <img id = "background" src = {"https://images.pexels.com/photos/2525903/pexels-photo-2525903.jpeg"}/>
+            <img id = "background" src = {destination?.destinationInfo?.back_url}/>
 
             <div className = "intro-info">
                 <div className = "dest-info">
-                    <h1 className = "destination-title"> Los Angeles, USA </h1>
+                    <h1 className = "destination-title"> {destination?.destinationInfo?.name} </h1>
                     <div className="loc-info">
-                        <p>{string}</p>
+                        <p>{destination?.destinationInfo?.description}</p>
                     </div>
                 </div>
                 <div className="map">
+                    {!map || destination?.length == 0 ? ( 
+                        <div className = "loading"> 
+                            <img  src={"https://i.gifer.com/origin/34/34338d26023e5515f6cc8969aa027bca_w200.gif"}/>
+                        </div>
+                    ) : ( <div></div> )}
                     <iframe
-                        width="550"
-                        height="500"
-                        referrerPolicy="no-referrer-when-downgrade"
-                        src={`https://www.google.com/maps/embed/v1/place?key=${apiKEY}&q=Los Angeles`}>
+                            width="550"
+                            height="500"
+                            referrerPolicy="no-referrer-when-downgrade"
+                            src={`https://www.google.com/maps/embed/v1/place?key=${apiKEY}&q=${destination?.destinationInfo?.name}`}>
                     </iframe>
                 </div>
             </div>
 
             <div className ="full-container">
                 <div className="infoAndActivities">
-                    <h2 className = "activities-title">Activities</h2>
-                    <p>There are plenty of activities that you can do in <span id="cityname">Los Angeles!</span> </p> 
-                    <p>Here are a few:</p>                    
+                    <Activities activities = {destination?.destinationActivities}/>
                 </div>
-
                 <div className="flightsAndHotels">
                     <div className="hotels">
-                        <Hotels/>
+                        <Hotels hotels = {destination?.hotels?.data}/>
                     </div>
                     <div className="flights">
                         <FlightForm/>
