74d3c56408b1ca07bf4bf4a0dd961cf01cff6b2d
emandaseifu <emandatseifu@gmail.com>
2023-07-24 16:55:38 -0400

    overall

--- exploreio-ui/src/components/Home/Home.jsx
+++ exploreio-ui/src/components/Home/Home.jsx
@@ -4,19 +4,19 @@ import Hero from "../Hero/Hero";
 import SlideCard from "../SlideCard/SlideCard";
 import SlideCard2 from "../SlideCard/SlideCard2";
 import SlideCard3 from "../SlideCard/SlideCard3";
-import Footer from "../Footer/Footer"; 
+import Footer from "../Footer/Footer";
 
 export default function Home() {
     return (
         <div>
             <div className="home">
-                <img id = "background-image" src=
-                "https://images.pexels.com/photos/16922741/pexels-photo-16922741/free-photo-of-a-travel-boat-in-prague.jpeg" 
-                alt="front" />     
+                <img id="background-image" src=
+                    "https://images.pexels.com/photos/10559103/pexels-photo-10559103.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                    alt="front" />
                 <div className="topPage">
                     <Hero />
                 </div>
-      
+
                 <div className="midPage">
                     <SlideCard />
                     <br></br>
@@ -26,11 +26,11 @@ export default function Home() {
                     <br></br>
                     <SlideCard3 />
                 </div>
-      
+
                 <div className="bottomPage">
                     <Footer />
                 </div>
-      
+
             </div>
         </div>
     );
