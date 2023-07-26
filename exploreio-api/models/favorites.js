4e86bf1e1ba1f12e04a49b2dee0ac8339011d7f4
aboss908 <aboss908@gmail.com>
2023-07-26 19:00:52 -0400

    Made lots of changes to incorporate favorites, and added logging in / registering functionality

---
+++ exploreio-api/models/favorites.js
@@ -0,0 +1,63 @@
+const db = require("../config/database")
+const { BadRequestError } = require("../utils/errors")
+
+class Favorites {
+    
+    /* Grabbing all of the favorites of one user. */
+    static grabFavoritesByID = async function(id) {
+        const query = `SELECT * FROM favorites WHERE UserID = $1`
+        const {rows} = await db.query(query,[id])
+        return rows
+    }
+
+    /* Adding a favorite */
+    static addFavorites = async function(destination, id) {
+        /* Searching to see if the favorite already exists for that person in the database. */
+        const searchQuery = `SELECT * FROM favorites WHERE (UserID = $1 AND name = $2)`
+        const {rows} = await db.query(searchQuery, [id, destination.name])
+
+        if (rows.length == 0) {
+            /* Inserting the new destination into favorites. */
+            const query = `INSERT INTO favorites (UserID, name, image, description, rating, region, country)
+            VALUES ($1, $2, $3, $4, $5, $6, $7) RETURNING *;`
+
+            const {rows} = await db.query(query, [id, destination.name, destination.image_url, destination.description,
+            destination.rating, destination.region, destination.country])
+
+            /* Returning the favorite. */
+            return rows[0]
+        } else {
+            throw new BadRequestError("Favorite already exists in the list")
+        }
+    }
+
+    static removeFavorites = async function(destination, id) {
+        /* Searching to see if the favorite exists in the database to be removed. */
+        const searchQuery = `SELECT * FROM favorites WHERE (UserID = $1 AND name = $2)`
+        const {rows} = await db.query(searchQuery, [id, destination.name])
+
+        /* If the favorite exists in the table, then remove it. */
+        if (rows.length != 0) {
+            const query = `DELETE FROM favorites WHERE (UserID = $1 AND name = $2)`
+            const {rows} = await db.query(query, [id, destination.name])
+
+            return rows[0]
+        } else {
+            throw new BadRequestError("Favorite doesn't exist")
+        }
+    }
+
+    /* Checking if a user has already favorited a destination */
+    static checkFavorited = async function(destination, id) {
+        const searchQuery = `SELECT * FROM favorites WHERE (UserID = $1 AND name = $2)`
+        const {rows} = await db.query(searchQuery, [id, destination.name])
+
+        if (rows.length != 0) {
+            return true
+        }
+
+        return false
+    }
+}
+
+module.exports = Favorites
