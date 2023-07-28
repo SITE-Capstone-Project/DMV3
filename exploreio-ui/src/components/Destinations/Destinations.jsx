82d53266063634db76cb80d83b269ba1de6bd452
emandaseifu <emandatseifu@gmail.com>
2023-07-28 12:14:57 -0400

    search

--- exploreio-ui/src/components/Destinations/Destinations.jsx
+++ exploreio-ui/src/components/Destinations/Destinations.jsx
@@ -2,23 +2,28 @@ import React, { useState, useEffect } from "react";
 import SearchBox from "../SearchBox/SearchBox";
 import DestinationContainer from "../DestinationContainer/DestinationContainer";
 import Footer from "../Footer/Footer";
-import { Container } from "react-bootstrap"; // Import Container from React Bootstrap
+import { Container } from "react-bootstrap";
 
 export default function Destinations({ destinations }) {
   const [filteredData, setFilteredData] = useState(destinations);
+  const [filterRating, setFilterRating] = useState("");
+  const [filterCost, setFilterCost] = useState(""); // Add state for filtering by cost
 
   useEffect(() => {
     setFilteredData(destinations);
   }, [destinations]);
 
-  const handleFilter = (searchTerm, filterOptions) => {
+  const handleFilter = (searchTerm, filterRating, filterCost) => {
     let filteredResults = destinations.filter((item) => {
       const nameMatch = item.name.toLowerCase().includes(searchTerm.toLowerCase());
 
       // Check if the rating is greater than or equal to the selected filter option
-      const ratingMatch = item.rating >= parseFloat(filterOptions);
+      const ratingMatch = item.rating >= parseFloat(filterRating);
 
-      return nameMatch && (!filterOptions || ratingMatch);
+      // Check if the cost_level matches the selected filter option
+      const costMatch = !filterCost || item.cost_level === filterCost;
+
+      return nameMatch && (!filterRating || ratingMatch) && (!filterCost || costMatch);
     });
 
     setFilteredData(filteredResults);
@@ -26,9 +31,14 @@ export default function Destinations({ destinations }) {
 
   return (
     <div>
-      <Container> {/* Wrap the content in a Container from React Bootstrap */}
+      <Container>
         <h1>Destinations</h1>
-        <SearchBox data={filteredData} onFilter={handleFilter} />
+        <SearchBox
+          data={filteredData}
+          onFilter={(searchTerm, filterRating, filterCost) =>
+            handleFilter(searchTerm, filterRating, filterCost)
+          } // Pass search term, filter rating, and filter cost to handleFilter
+        />
         <DestinationContainer destinations={filteredData} />
         <Footer />
       </Container>
--- exploreio-ui/src/components/SearchBox/SearchBox.jsx
+++ exploreio-ui/src/components/SearchBox/SearchBox.jsx
@@ -1,17 +1,23 @@
 import React, { useState } from "react";
 
-export default function SearchBox({ onFilter }) {
+export default function SearchBox({ data, onFilter }) {
   const [searchTerm, setSearchTerm] = useState("");
-  const [filterOptions, setFilterOptions] = useState(""); // Add state for filtering options
+  const [filterRating, setFilterRating] = useState("");
+  const [filterCost, setFilterCost] = useState(""); 
 
   const handleInputChange = (event) => {
     setSearchTerm(event.target.value);
-    onFilter(event.target.value, filterOptions); // Pass search term and filter options to the onFilter function
+    onFilter(event.target.value, filterRating, filterCost); 
   };
 
-  const handleFilterChange = (event) => {
-    setFilterOptions(event.target.value);
-    onFilter(searchTerm, event.target.value); // Pass search term and filter options to the onFilter function
+  const handleFilterRatingChange = (event) => {
+    setFilterRating(event.target.value);
+    onFilter(searchTerm, event.target.value, filterCost); 
+  };
+
+  const handleFilterCostChange = (event) => {
+    setFilterCost(event.target.value);
+    onFilter(searchTerm, filterRating, event.target.value);
   };
 
   return (
@@ -23,13 +29,21 @@ export default function SearchBox({ onFilter }) {
         onChange={handleInputChange}
       />
 
-      <select value={filterOptions} onChange={handleFilterChange}>
+      <select value={filterRating} onChange={handleFilterRatingChange}>
         <option value="">Filter by rating</option>
         <option value="1">1+</option>
         <option value="2">2+</option>
         <option value="3">3+</option>
         <option value="4">4+</option>
         <option value="5">5</option>
+      </select>
+
+      <select value={filterCost} onChange={handleFilterCostChange}>
+        <option value="">Filter by cost</option>
+        <option value="$">$</option>
+        <option value="$$">$$</option>
+        <option value="$$$">$$$</option>
+        <option value="$$$$">$$$$</option>
       </select>
     </div>
   );
