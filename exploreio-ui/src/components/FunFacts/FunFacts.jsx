def128955b92e1f69627e554ddddeacd1d26c08d
aboss908 <aboss908@gmail.com>
2023-08-03 19:29:49 -0400

    More modifications

--- exploreio-ui/src/components/FunFacts/FunFacts.jsx
+++ exploreio-ui/src/components/FunFacts/FunFacts.jsx
@@ -6,7 +6,19 @@ export default function FunFacts({ facts }) {
     return (
         <div className = "fun-facts">
             <h2 className = "facts-title">Fun Facts</h2>
-            
+            {facts?.map((element, index) => {
+                return <Fact 
+                key = {element + index}
+                fact = {element}/>
+            })}
+        </div>
+    )
+}
+
+export function Fact({fact}) {
+    return (
+        <div className = "fact-card">
+            <p id ="fact"> {fact} </p>
         </div>
     )
 }
