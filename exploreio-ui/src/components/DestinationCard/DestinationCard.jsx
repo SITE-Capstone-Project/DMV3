a7833ee18797bbf896723cd078f71296b839dad7
emandaseifu <emandatseifu@gmail.com>
2023-08-03 16:18:49 -0400

    stars

--- exploreio-ui/src/components/DestinationCard/DestinationCard.jsx
+++ exploreio-ui/src/components/DestinationCard/DestinationCard.jsx
@@ -3,8 +3,6 @@ import React from "react";
 import { Link } from "react-router-dom";
 import logo from "../../assets/logo.png";
 
-// ... (imports and other code)
-
 export default function DestinationCard({ id, name, rating, image }) {
   const formattedRating = rating.toFixed(1);
 
